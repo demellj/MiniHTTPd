ddc48d7575dc224a7b56985d8b058d3360a97a49
John Mark de Mello <demellj@users.noreply.github.com>
2019-11-15 01:06:46 -0500

    Revert "Move accept to its own thread"
    This reverts commit 40a04da6

--- src/demellj/minihttpd/Worker.java
+++ src/demellj/minihttpd/Worker.java
@@ -16,13 +16,13 @@ public class Worker implements Runnable {
     private final AtomicBoolean isRunning;
     private final Selector selector;
     private final ThreadSafeResponder responder;
-    private final WorkerSync sync;
+    private final Object sync;
 
     Worker(ConcurrentHashMap<SocketChannel, Client> sessions,
            AtomicBoolean isRunning,
            Selector selector,
            ThreadSafeResponder responder,
-           WorkerSync sync) {
+           Object sync) {
         this.sessions = sessions;
         this.isRunning = isRunning;
         this.selector = selector;
@@ -36,8 +36,6 @@ public class Worker implements Runnable {
             try {
                 SelectableChannel ch = null;
 
-                sync.signalAndWait();
-
                 // Synchronize on 'sync', simply because it is shared by all workers.
                 // Attempting to ensure a consistent view of selectedKeys.
                 synchronized (sync) {
@@ -46,12 +44,19 @@ public class Worker implements Runnable {
                         for (final SelectionKey key : selectedKeys) {
                             boolean needsClosing = false;
                             try {
-                                if (key.isValid() && key.isReadable()) {
-                                    ch = key.channel();
+                                if (key.isValid()) {
+                                    // Perform "accept" while holding lock, to reduce unnecessary
+                                    // contention between workers when a client connects.
+                                    if (key.isAcceptable())
+                                        performClientAccept((ServerSocketChannel) key.channel());
+
+                                    if (key.isReadable())
+                                        ch = key.channel();
+
+                                    selectedKeys.remove(key);
                                 } else {
                                     needsClosing = true;
                                 }
-                                selectedKeys.remove(key);
                             } catch (CancelledKeyException ignored) {
                                 needsClosing = true;
                             }
@@ -73,6 +78,19 @@ public class Worker implements Runnable {
             } catch (Exception e) {
                 e.printStackTrace();
             }
+        }
+    }
+
+    private void performClientAccept(ServerSocketChannel serverChannel) {
+        try {
+            final SocketChannel chan = serverChannel.accept();
+            if (chan != null && sessions.putIfAbsent(chan, new Client(chan)) == null) {
+                chan.configureBlocking(false);
+                chan.register(selector, SelectionKey.OP_READ);
+                System.err.println(String.format("%s connected", chan.getRemoteAddress()));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
